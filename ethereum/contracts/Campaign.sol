d31ddfe0c6567a379989869b06a7ba55bda777fe
David Sotto Mayor <ddsmmf@gmail.com>
2024-08-22 18:26:07 -0400

    Creating the contribute function

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -3,12 +3,22 @@ pragma solidity ^0.4.17;
 contract Campaign {
     address public manager;
     uint public minimumAmount;
+    address[] public approvers;
 
     // Once someone raise the contract, we want to... 
     function Campaign (uint minimum) {
         // define who is the contract owner
         manager = msg.sender;
-        // define the minimum amount to contribuite
+        // define the minimum amount to contribute
         minimumAmount = minimum;
+    }
+
+    // Once someone contribute to the contract, we want to...
+    function contribute() public payable { //It is payable because this function receives some amount of Ether
+        // Check if the incomming amount is grather than the minimum amount
+        require(msg.value > minimumAmount);
+
+        // Add the address of the donator into the approvers list.
+        approvers.push(msg.sender);
     }
 }
