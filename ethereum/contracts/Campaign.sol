f781e4bfd09fb0a06b3473a4dbca9d2c0bade9d2
David Sotto Mayor <ddsmmf@gmail.com>
2024-08-22 18:20:50 -0400

    Setting up the construct

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,3 +1,14 @@
 pragma solidity ^0.4.17;
 
-contract Campaign {}
+contract Campaign {
+    address public manager;
+    uint public minimumAmount;
+
+    // Once someone raise the contract, we want to... 
+    function Campaign (uint minimum) {
+        // define who is the contract owner
+        manager = msg.sender;
+        // define the minimum amount to contribuite
+        minimumAmount = minimum;
+    }
+}
