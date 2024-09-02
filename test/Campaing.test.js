f8260500d1d3bb066cf605b08ebbeacc76cd8c14
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-02 14:54:49 -0400

    Check if the manager is setting up

--- test/Campaing.test.js
+++ test/Campaing.test.js
@@ -5,7 +5,7 @@ const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
-const { describe } = require('mocha');
+const { describe, it } = require('mocha');
 
 let accounts;
 let factory;
@@ -43,6 +43,12 @@ describe('Campaigns', () => {
     it('deploys the contracts', async () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    })
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = campaign.methods.manager().call();
+        
+        assert.equal(accounts[0], manager);
     })
 
 })
