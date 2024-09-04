81f7c2b907ae7d0b310c18f6edfe909b78eac7f5
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-04 14:05:47 -0400

    Setting an instance to contract Factory

---
+++ ethereum/factory.js
@@ -0,0 +1,12 @@
+import CampaingFactory from './build/CampaignFactory.json';
+import web3 from './web3';
+require('dotenv').config()
+
+const instance = new web3
+.eth
+.Contract(
+    JSON.parse(CampaingFactory.interface), 
+    process.env.SMART_CONTRACT_ADDRESS
+)
+
+export default instance
