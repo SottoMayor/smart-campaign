0d218b2dd9b2c8218dd97f8673d12f4152839a6d
SottoMayor <ddsmmf@gmail.com>
2024-09-20 10:50:17 -0400

    Handling form submition and connecting the web3 features

--- pages/campaings/new.js
+++ pages/campaings/new.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 import { Form, Button, Input } from 'semantic-ui-react'
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 const CampaingNew = () => {
     const [amount, setAmount] = useState(0);
 
     const amountHandler = (event) => setAmount(event.target.value)
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        await factory
+        .methods
+        .createCampaing(amount)
+        .send({
+            from: accounts[0],
+            //gas: '' // Metamask handles it for us!
+        })
+    }
+
   return (
     <Layout>
         <h3>New Campaing!</h3>
 
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Form.Field>
                 <label>Minimum Ether Contribution</label>
                 <Input value={amount} onChange={amountHandler} label="amount in WEI" labelPosition='right'/>
