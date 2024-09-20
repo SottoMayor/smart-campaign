2b2290eaae8c2c39af4fdacabc962b89ea1c4a4f
SottoMayor <ddsmmf@gmail.com>
2024-09-20 10:33:25 -0400

    Adding the form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "826415a3af3bb537"; }
+/******/ 		__webpack_require__.h = function() { return "42e422693d25bfcb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaings/new.js
+++ pages/campaings/new.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import { Form, Button } from 'semantic-ui-react'
 
 const CampaingNew = () => {
   return (
     <Layout>
-        <div>CampaingNew</div>
+        <h3>New Campaing!</h3>
+
+        <Form>
+            <Form.Field>
+                <label>Minimum Contribution</label>
+                <input/>
+            </Form.Field>
+
+            <Button primary>Create!</Button>
+        </Form>
     </Layout>
   )
 }
