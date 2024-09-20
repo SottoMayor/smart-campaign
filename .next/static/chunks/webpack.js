657c8fcdbfa5f70a59204248b8577f1cce4344aa
SottoMayor <ddsmmf@gmail.com>
2024-09-20 10:08:29 -0400

    Creating the /campaign/new page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6bf09cabb60e123"; }
+/******/ 		__webpack_require__.h = function() { return "826415a3af3bb537"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/campaings/new.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const CampaingNew = () => {
+  return (
+    <Layout>
+        <div>CampaingNew</div>
+    </Layout>
+  )
+}
+
+export default CampaingNew
