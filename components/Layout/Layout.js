a858f2713cc648760d26cb082309992b495f6416
SottoMayor <ddsmmf@gmail.com>
2024-09-20 10:07:59 -0400

    Moving the semantic-ui-css to an appropriate place (Layout component)

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import { Container } from 'semantic-ui-react';
 import Footer from './Footer';
+import 'semantic-ui-css/semantic.min.css'
 
 const Layout = ({ children }) => {
   return (
