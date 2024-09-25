284e06ea955f123b71441a24e7b58a388bbd2191
SottoMayor <ddsmmf@gmail.com>
2024-09-25 08:51:18 -0400

    Hidrating the datails page with contract campaign data

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -14,10 +14,16 @@ const CampaignDetails = () => {
 CampaignDetails.getInitialProps = async (props) => {
  const campaign = campaignInstance(props.query.address);
 
+ // Returns an object, your keys are non-negative intergers
  const summary = await campaign.methods.getSummary().call();
 
- console.log(summary)
- return {}
+ return {
+  minimumContribution: summary[0],
+  balance: summary[1],
+  requestsCount: summary[2],
+  approversCount: summary[3],
+  manager: summary[4],
+ }
 }
 
 export default CampaignDetails;
