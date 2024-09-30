2e6399ceb9552ce7f697400e98ad246fa2fc614d
SottoMayor <ddsmmf@gmail.com>
2024-09-30 14:17:10 -0400

    Handling in the form the amount to be donate

--- components/Contribute.js
+++ components/Contribute.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 
 const Contribute = () => {
+    const [value, setValue] = useState(0);
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        
+    }
+
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
         <Form.Field>
             <label>Amount to Contribute</label>
-            <Input label="Ether" labelPosition='right'/>
+            <Input 
+            value={value}
+            onChange={event => setValue(event.target.value)}
+            label="Ether" labelPosition='right'/>
         </Form.Field>
         <Button primary>Contribute</Button>
     </Form>
