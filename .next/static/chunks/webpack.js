bf0f6acf90d1429eff766aa868549265ac3ecf8a
SottoMayor <ddsmmf@gmail.com>
2024-09-30 14:35:49 -0400

    Connecting the web3 and allowing to contribute

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93fde1bcf78960ce"; }
+/******/ 		__webpack_require__.h = function() { return "51382848bcef88b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Contribute.js
+++ components/Contribute.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
-const Contribute = () => {
+const Contribute = (props) => {
     const [value, setValue] = useState(0);
 
-    const submitHandler = event => {
+    const submitHandler = async (event) => {
         event.preventDefault();
 
-        
+        const campaign = Campaign(props.address)
+
+        try{
+            const accounts = await web3.eth.getAccounts()
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(value, 'ether')
+            });
+        }catch(error){
+
+        }
     }
 
   return (
