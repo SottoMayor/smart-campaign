ba98d44fd3202f03acabbd32e9d6af8a9ef4d958
SottoMayor <ddsmmf@gmail.com>
2024-10-01 16:20:35 -0400

    Allowing form submition, connecting with the web3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1c762523e790fc27"; }
+/******/ 		__webpack_require__.h = function() { return "76e39f1a962a1849"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -1,31 +1,58 @@
 import React, { useState } from 'react'
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Campaign from '../../../../ethereum/campaign';
+import web3 from '../../../../ethereum/web3';
+import Link from 'next/link';
+import Router from 'next/router';
 import Layout from '../../../../components/Layout/Layout';
 
-const newRequest = () => {
+const newRequest = (props) => {
 
   const [value, setValue] = useState('');
   const [description, setDescription] = useState('');
   const [recipient, setRecipient] = useState('');
   
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const campaign = Campaign(props.address);
+
+    console.log('ok')
+
+    try{
+      const accounts = await web3.eth.getAccounts();
+      await campaign
+      .methods
+      .createRequest(
+        description,
+        web3.utils.toWei(value, 'ether'),
+        recipient
+      )
+      .send({ from: accounts[0] })
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return (
     <Layout>
       <h3>Create a Request</h3>
 
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Field>
           <label>Description</label>
           <Input 
-          value={value}
-          onChange={event => setValue(event.target.value)}
+          value={description}
+          onChange={event => setDescription(event.target.value)}
           />
         </Form.Field>
 
         <Form.Field>
           <label>Value in Ether</label>
           <Input
-          value={description}
-          onChange={event => setDescription(event.target.value)}
+          value={value}
+          onChange={event => setValue(event.target.value)}
           />
         </Form.Field>
 
