e6e70c78c4c9ecbfa8f698cd9f5983c7c07868c8
SottoMayor <ddsmmf@gmail.com>
2024-10-03 16:22:50 -0400

    Creating the request table component

---
+++ components/RequestTable.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import RequestRow from './RequestRow';
+import { Table } from 'semantic-ui-react';
+
+const RequestTable = (props) => {
+
+    const { Header, Row, HeaderCell, Body } = Table
+
+    const renderRows = () => {
+      return props.reqs.map((req, index) => (
+        <RequestRow
+        request={req}
+        id={index}
+        key={index}
+        address={props.address}
+        />
+      ))
+    }
+
+  return (
+    <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+          </Row>
+        </Header>
+        <Body>
+          {renderRows()}
+        </Body>
+    </Table>
+  )
+}
+
+export default RequestTable
