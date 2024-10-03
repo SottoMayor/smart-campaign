6321e23de0af670d57b725ba39a40d704210e8c1
SottoMayor <ddsmmf@gmail.com>
2024-10-03 16:22:33 -0400

    Creating the request row component

---
+++ components/RequestRow.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+
+const RequestRow = (props) => {
+
+    const { Row, Cell } = Table
+    const { id, request: { description, value, recipient, complete, approvalCount } } = props
+
+  return (
+    <Row>
+        <Cell>{id + 1}</Cell>
+        <Cell>{description}</Cell>
+        <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
+        <Cell>{recipient}</Cell>
+        <Cell>{approvalCount}</Cell>
+        <Cell>{complete ? 'true' : 'false'}</Cell>
+        <Cell>COMPLETE!!!</Cell>
+    </Row>
+  )
+}
+
+export default RequestRow
